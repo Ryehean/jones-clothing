a62999b9c9b178cbb0709b60eb92dccb18032f93
Ryehean Jones <Ryehean@gmail.com>
2022-07-24 13:04:50 -0400

    Added our sign in component as well as Firebase db

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import CategoryItem from '../category-item/category-item.component';
-import './categories-menu.syles.scss';
+import './directory.styles.scss';
 
-const CategoriesMenu = ({categories}) => {
+const Directory = ({categories}) => {
     return (
         <div className="categories-container">
             {categories.map((category) => (
@@ -11,4 +11,4 @@ const CategoriesMenu = ({categories}) => {
     );
 }
 
-export default CategoriesMenu;
+export default Directory;
